b7a43c066bc6bcdc75ebb910835d395aa6e59b83
Cool Dumbeldore <cool.dumbeldore@yandex.com>
2016-06-27 21:00:08 +0300

    updated code for recursive find_links

--- get_links_from_video.py
+++ get_links_from_video.py
@@ -2,20 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-youtube_url = "https://www.youtube.com/watch?v=TNN9MnbhqKY"
+# youtube_url = "https://www.youtube.com/watch?v=TNN9MnbhqKY"
 
-content = requests.get(youtube_url).text
+def find_links(youtube_url):
+	content = requests.get(youtube_url).text
 
-soup = BeautifulSoup(content, 'html.parser')
+	soup = BeautifulSoup(content, 'html.parser')
 
-a_links = soup.find_all("a", class_="yt-uix-sessionlink")
+	a_links = soup.find_all("a", class_="yt-uix-sessionlink")
 
-hrefs = [link['href'] for link in a_links]
+	hrefs = [link['href'] for link in a_links]
 
-video_links = []
-for href in hrefs:
-	if href[0:6] == "/watch":
-		video_links.append(href)
+	video_links = []
+	for href in hrefs:
+		if href[0:6] == "/watch":
+			video_links.append(href)
 
-print video_links
+	return video_links
 
+def find_links_recursivly(base_url, depth):
+	if depth == 0:
+		return []
+
+	urls = find_links(base_url)
+	links = urls
+	for url in urls:
+		links += find_links_recursivly(url, depth - 1)
+
+	return links
